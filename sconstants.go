1746127a3253dbc343ab698cbe478882c79ca4fb
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-07-18 15:43:03 -0700

    s constants

---
+++ sconstants.go
@@ -0,0 +1,22 @@
+package md4
+
+
+// These constants are defined IETF RFC-1320.
+//
+// https://datatracker.ietf.org/doc/html/rfc1320
+//
+// And, are used in the md4Transform function.
+const (
+	s11 = 3
+	s12 = 7
+	s13 = 11
+	s14 = 19
+	s21 = 3
+	s22 = 5
+	s23 = 9
+	s24 = 13
+	s31 = 3
+	s32 = 9
+	s33 = 11
+	s34 = 15
+)
