05d78d2ff6a5966f6fc47a9c09ebd0ae6d6b1c4b
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-07-19 11:05:12 -0700

    corrected typo in docs

--- sum.go
+++ sum.go
@@ -1,6 +1,6 @@
 package md4
 
-// Sum returns the MD4 digest (checksum) of `data` calculating using the MD4 hash-function.
+// Sum returns the MD4 digest (checksum) of `data` calculated using the MD4 hash-function.
 func Sum(data []byte) [Size]byte {
 	var h hasher
         h.Reset()
